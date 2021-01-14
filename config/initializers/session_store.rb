07682b9b07982c82b58cd5192e517d2fe5f209b1
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-01-14 01:11:58 -0500

    updated app to run in production and development environtment

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "YOUR-DOMAIN-NAME"
+if Rails.env == 'production'
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "YOUR-DOMAIN-NAME"
+else
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+end
